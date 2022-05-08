1f2bd5df8fd811602f63b4c1857e8ece4e7dfbb0
zuitopia <korhwngml123@naver.com>
2022-05-09 05:50:56 +0900

    수정

--- js/todolist.js
+++ js/todolist.js
@@ -5,7 +5,7 @@ const todoList = document.querySelector("#todolist");
 const todoSet = [];
 
 function savedTodoArray(){
-    localStorage.setItem("todoArray", todoArray); 
+    localStorage.setItem("todoSet", todoSet);
 }
 
 function removeTodo(){
@@ -13,18 +13,25 @@ function removeTodo(){
     removeList.remove();
 }
 
+function createTodoElement(newTodo){
+    const span = document.createElement("span"); 
+    span.innerText = newTodo;
+    return span;
+}
+
+function createDeleteButton(){
+    const deleteButton = document.createElement("button");
+    deleteButton.addEventListener("click", removeTodo);
+    deleteButton.innerText = "x";
+    return deleteButton;
+}
 
 function showTodo(newTodo){
     const li = document.createElement("li");
-    const span = document.createElement("span"); 
-    span.innerText = newTodo;
-    
-    const button = document.createElement("button");
-    button.addEventListener("click", removeTodo);
-    button.innerText = "X";
-    
+    const span = createTodoElement(newTodo);
+    const deleteButton = createDeleteButton();
     li.appendChild(span);
-    li.appendChild(button);
+    li.appendChild(deleteButton);
     todoList.appendChild(li);
 }
 
@@ -34,8 +41,18 @@ function handleTodo(event){
     const newTodo = todoInput.value;
     todoInput.value = "";
     todoSet.push(newTodo);
-    showTodo(newTodo);
-    savedTodoArray();
+    showTodo(newTodo); // 받은 newTodo 화면에 출력
+    savedTodoArray(); // 입력받은 newTodo들을 local storage에 저장
 } 
 
+
 todoForm.addEventListener("submit", handleTodo);
+
+function showAreadyExist() // 반응 없는 함수, 수정 필요
+{
+    if(removeList == null){
+        localStorage.getItem("todoSet");
+    }else{
+        removeTodo();
+    }
+}
